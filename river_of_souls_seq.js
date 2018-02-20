61120db9239056f8b7cd55c9e1b7fd2d9c2d0194
R.A.Robertson <RARioRA@IO.local>
2018-02-20 17:59:55 -0600

    Add inter-annular radiance

--- river_of_souls_seq.js
+++ river_of_souls_seq.js
@@ -10,14 +10,14 @@ function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent();
   bground = color(255);										                  // Color definitions.
-  onionSkin = color(255, 30);
-  soulStroke = color(214, 225);
+  onionSkin = color(20, 30);
+  soulStroke = color(214, 150);
   soulFill = color(0, 126, 255, 1);
   background(bground);
   v = 2;
   windowRes = windowWidth * windowHeight;
   // soulNum = int(windowRes / 30000);
-                                              soulNum = 15; // Dev testing. Update windowResized also.
+                                              soulNum = 12; // Dev testing. Update windowResized also.
   // soulNum = constrain(soulNum, 12, 33);
   for (var i = 0; i < soulNum; i++) {												 // Initialize souls.
     souls[i] = new Soul();                // ??? Look this up.
@@ -45,31 +45,26 @@ function draw() {
 
 
         if (soulsDistance <= communionDistance) {		          					          // Communion.
-          var lineWeight = 15/soulsDistance;
-          commLines = color(lineWeight * 80, lineWeight, lineWeight, 50);			    // Communion lines color.
-strokeWeight(lineWeight);
-          stroke(commLines, lineWeight);
-          line(souls[i].x, souls[i].y, souls[j].x, souls[j].y);
+          // var lineWeight = 15/soulsDistance;
+          // commLines = color(lineWeight * 80, lineWeight, lineWeight, 50);			    // Communion lines color.
+          // strokeWeight(lineWeight);
+          // stroke(commLines, lineWeight);
+          // line(souls[i].x, souls[i].y, souls[j].x, souls[j].y);
 
 
 // New code mostly here.
 
-        // var radFract, limit, lerpX, lerpY;
-        // radFract = (souls[i].d / 2) / soulsDistance;
-        //
-        // for (var k = 1; k < souls[i].d; k++) {    // Gradient loop.
-        //   limit = (soulsDistance > souls[i].d / 2) ? k/souls[i].d * radFract : k/souls[i].d;
-        //   lerpX = lerp(souls[i].x, souls[j].x, limit);
-        //   lerpY = lerp(souls[i].y, souls[j].y, limit);
-        //   noStroke();
-        //   fill(k/1.5 * int(255 / souls[i].d), k * int(255 / souls[i].d) );
-        //   // fill(k/1.5 * int(255 / souls[i].d), lineWeight * k );
-        //   ellipse(lerpX, lerpY, souls[i].d - k, souls[i].d - k);
-        // } // End gradient.
-        //
-        // fill(onionSkin, 255/ (communionDistance - soulsDistance) );
-        // ellipse(souls[i].x, souls[i].y, souls[i].d, souls[i].d);
+        var radFract, limit, lerpX, lerpY;
+        radFract = (souls[i].d / 2) / soulsDistance;
 
+        for (var k = 1; k < souls[i].d; k++) {    // Gradient loop.
+          limit = (soulsDistance > souls[i].d / 2) ? k/souls[i].d * radFract : k/souls[i].d;
+          lerpX = lerp(souls[i].x, souls[j].x, limit);
+          lerpY = lerp(souls[i].y, souls[j].y, limit);
+          noStroke();
+          fill(k/1.5 * int(255 / souls[i].d), k * int(255 / souls[i].d) );  // Tweak alpha for interest.
+          ellipse(lerpX, lerpY, souls[i].d - k, souls[i].d - k);
+        } // End gradient.
 
 
         // This may or may not be useful. Still testing. Would like to be able to fade in orbs.
@@ -85,23 +80,42 @@ strokeWeight(lineWeight);
         // strokeWeight(4);
         var v1 = createVector(souls[i].x, souls[i].y);
         var v2 = createVector(souls[j].x, souls[j].y);
-        // var centersDistVal = p5.Vector.dist(v1, v2);	   // Get distance
-        var radFract1 = (souls[i].d/2) / soulsDistance;			   // Get fraction radius1:dist
-        var radFract2 = (souls[j].d/2) / soulsDistance;				 // Get fraction radius2:dist
-        var lerpV1 = p5.Vector.lerp(v1, v2, radFract1);	 // Interpolate per fraction1
-        var lerpV2 = p5.Vector.lerp(v2, v1, radFract2);	 // Interpolate per fraction2
+        // var centersDistVal = p5.Vector.dist(v1, v2);	            // Get distance
+        var radFract1 = (souls[i].d/2) / soulsDistance;			      // Get fraction radius1:dist
+        var radFract2 = (souls[j].d/2) / soulsDistance;           // Get fraction radius2:dist
+        var lerpV1 = p5.Vector.lerp(v1, v2, radFract1);	          // Interpolate per fraction1
+        var lerpV2 = p5.Vector.lerp(v2, v1, radFract2);	          // Interpolate per fraction2
         stroke('red');
-        ellipse(lerpV1.x, lerpV1.y, 4, 4);						 // Show perimeter near points
-        ellipse(lerpV2.x, lerpV2.y, 4, 4);
-        var v3 = createVector(lerpV1.x, lerpV1.y);			 // Location of perimeter point1
-        var v4 = createVector(lerpV2.x, lerpV2.y);       // Location of perimeter point2
-        var lerpPerimeters = p5.Vector.lerp(v3, v4, 0.5); // Set pt along perimeter line
+        // ellipse(lerpV1.x, lerpV1.y, 4, 4);						            // Show perimeter near points
+        // ellipse(lerpV2.x, lerpV2.y, 4, 4);
+        var v3 = createVector(lerpV1.x, lerpV1.y);			          // Location of perimeter point1
+        var v4 = createVector(lerpV2.x, lerpV2.y);                // Location of perimeter point2
+        var lerpPerimeters = p5.Vector.lerp(v3, v4, 0.5);         // Set pt along perimeter line
         line(souls[i].x, souls[i].y, souls[j.x], souls[j].y);
         var radAverage = (souls[i].d + souls[j].d) / 2;
         radAverage = map(soulsDistance, communionDistance, 0,  0, radAverage);
-        stroke(color('blue'));
-        ellipse(lerpPerimeters.x, lerpPerimeters.y, radAverage, radAverage);
+        // stroke(color('blue'));
+        // ellipse(lerpPerimeters.x, lerpPerimeters.y, radAverage, radAverage);
 
+
+
+stroke(255, radAverage/32);
+strokeWeight(1);
+var r = radAverage;
+var gain = 3;
+for (var l = 0; l < 150; l++) {
+  var x1 = randomGaussian(souls[i].x, (l + r) / gain);
+  var y1 = randomGaussian(souls[i].y, (l + r) / gain);
+  var x2 = randomGaussian(souls[j].x, (l + r) / gain);
+  var y2 = randomGaussian(souls[j].y, (l + r) / gain);
+  line(x1, y1, lerpPerimeters.x, lerpPerimeters.y);
+  line(x2, y2, lerpPerimeters.x, lerpPerimeters.y);
+
+  // Or, the simpler way:
+  // var x1 = randomGaussian(lerpPerimeters.x, (l + r) / gain);
+  // var y1 = randomGaussian(lerpPerimeters.y, (l + r) / gain);
+  // line(x1, y1, lerpPerimeters.x, lerpPerimeters.y);
+}
 
 
 
@@ -145,9 +159,9 @@ strokeWeight(lineWeight);
 
 // ====================== Soul Object ====================== //
 
-function Soul() {											 // Setup.
+function Soul() {											                       // Setup.
   this.d = int(random(10, windowRes * 0.00015));			       // Adaptive/responsive sizing.
-  this.d = constrain(this.d, 10, int(random(10, 150)));
+  this.d = constrain(this.d, 30, int(random(30, 150)));
   this.x = random(this.d, windowWidth - this.d);
   this.y = random(this.d, windowHeight - this.d);
   this.xv = random(-v, v);
@@ -155,7 +169,7 @@ function Soul() {											 // Setup.
 
   this.show = function() {									                 // Display.
     stroke(soulStroke);
-    strokeWeight(2);
+    strokeWeight(.2);
     // fill(soulFill);
     ellipse(this.x, this.y, this.d, this.d);
   }
